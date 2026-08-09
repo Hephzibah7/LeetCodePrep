77b95b766ad15b815e91475942a8e8429a9d672b
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-08-09 23:42:28 +0530

    revision required must- difference array learnt

---
+++ Contest3rdQuestions/MinimumInitialStrengthToDefaeatAllMonsters.java
@@ -0,0 +1,42 @@
+/*
+https://leetcode.com/problems/minimum-initial-strength-to-defeat-all-monsters/
+Revision required must here
+
+Suppose you are given array of array something like this-
+[[1,2,3],[2,3,5]]-> where each array inside represnts range and the value to be added in the range,
+in manual manner, you will loop through each array and then loop through each range in that array which will give you a time complexity
+of O(n^2), and if you constraints are high will result in tle, so always maintain a difference array,
+and then prefix it, to get the correct values in proper index like this-
+boosts=[[2,4,7],[0,3,2]]
+for first array-[+7,0,0,0,-7,0] 
+for second array-[+2,0,+7,0,-2,-7] <-this is called diffrence array
+and prefix sum of difference array-[2,2,9,9,7,0]
+
+In the below question we are going or traversing backwards because
+if you are standing at index i, if subsequence indexes that is later indexes i+1..., require required energy you add
+monsters[i], otherwise if res if equal to 0, you need to add monsters[i]-bonus[i]
+
+ */
+package Contest3rdQuestions;
+
+public class MinimumInitialStrengthToDefaeatAllMonsters {
+    public long minInitialStrength(int[] monsters, int[][] boosts) {
+        int n=monsters.length;
+        long diff[]=new long[n];
+        for(int[] boost:boosts){
+            int l=boost[0];
+            int r=boost[1];
+            int v=boost[2];
+            diff[r]+=v;
+            if(l>0) diff[l-1]-=v;
+        }
+        long res=0;
+        long bonus=0;
+        for(int i=n-1; i>=0; i--){
+            bonus+=diff[i];
+            if(res>0) res+=monsters[i];
+            else res+=Math.max(0L,monsters[i]-bonus);
+        }
+        return res;
+    }
+}
