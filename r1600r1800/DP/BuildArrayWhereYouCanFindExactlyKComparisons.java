b2a139b8b6524af81ef4497e2c271cfe53037ce3
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-08-11 15:18:05 +0530

    red

--- r1600r1800/DP/BuildArrayWhereYouCanFindExactlyKComparisons.java
+++ r1600r1800/DP/BuildArrayWhereYouCanFindExactlyKComparisons.java
@@ -1,10 +1,40 @@
 /*
 https://leetcode.com/problems/build-array-where-you-can-find-the-maximum-exactly-k-comparisons/
+
+Bottom up or tabulation
+
+At position i,
+we have two options
+1) The new element updates the current maximum and becomes new maximum
+2)The new element does not update the current maximum which means the new element is less than j, if j is the current maximum
+
+ public int numOfArrays(int n, int m, int k) {
+        int mod=1000000007;
+        long dp[][][]=new long[n+1][m+1][k+1];
+        
+        //base case
+        for(int i=1; i<=m; i++) dp[1][i][1]=1;
+
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                for(int t=1; t<=k; t++){
+
+                    dp[i][j][t]=(dp[i][j][t]+(1L*j*dp[i-1][j][t])%mod)%mod;
+
+                    for(int x=1; x<j; x++) dp[i][j][t]=(dp[i][j][t]+dp[i-1][x][t-1])%mod;
+                }
+            }
+        }
+       long ans=0;
+       for(int j=1; j<=m; j++) ans=(ans+dp[n][j][k])%mod;
+       return (int)ans;
+    }
  */
 package r1600r1800.DP;
 
 import java.util.Arrays;
 
+//top-down approach
 public class BuildArrayWhereYouCanFindExactlyKComparisons {
     int mod=1000000007;
     public int numOfArrays(int n, int m, int k) {
