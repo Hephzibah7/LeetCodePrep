b8a89d646e8f79cfd3d25745b3501523b2237826
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-08-11 12:43:05 +0530

    yellow

--- r1600r1800/DP/KInversePairsArray.java
+++ r1600r1800/DP/KInversePairsArray.java
@@ -1,3 +1,47 @@
+/*
+https://leetcode.com/problems/k-inverse-pairs-array/
+K inverse Pair Array
+dp[n][k]=number of different arrays consisting of 0...n numbers and k inverse pairs
+
+
+dp[n][k]=dp[n-1][k]+dp[n-1][k-2]....dp[n-1][0] <-subproblem
+
+Suppose n=3
+1,2,3
+So according to fourmulae dp[3][2]=dp[2][0]+dp[2][1]+dp[2][2]
+
+dp[2][2] is not possible
+n=3 and k=2  3,1,2   1,2->dp[2][0]
+2,3,1->dp[2][1]
+
+Total 2
+
+Base case
+dp[0][0]=1
+dp[1][0]=1 1,2
+dp[2][0]=1 1,2,3
+
+n=1
+dp[1][0]=dp[0][0]=1
+n=2
+dp[2][1]=dp[1][0]+dp[1][1]=1  2,1
+
+
+
+Code-
+dp[0][0]=1;
+for(int i=1; i<=n; i++){
+	for(int j=0; j<=k; j++){
+		for(int index=0; index<=min(j,n-1); index++) dp[i][j]+=dp[i-1][j-index];
+}
+}
+
+
+
+
+
+
+*/
 package r1600r1800.DP;
 
 public class KInversePairsArray {
