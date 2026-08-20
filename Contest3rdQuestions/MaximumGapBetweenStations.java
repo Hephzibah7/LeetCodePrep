6477cd9c5e39e1bd72d281bc157f44933aa2d9b7
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-08-20 17:28:47 +0530

    red

---
+++ Contest3rdQuestions/MaximumGapBetweenStations.java
@@ -0,0 +1,36 @@
+package Contest3rdQuestions;
+
+public class MaximumGapBetweenStations {
+    public int maximumGap(String skill, String station) {
+        int n = skill.length();
+        int m = station.length();
+        if (n == 1)
+            return 0;
+        int start[] = new int[n];
+        int end[] = new int[n];
+        int i = n - 1;
+        int j = m - 1;
+        while (i >= 0 && j >= 0) {
+            if (skill.charAt(i) == station.charAt(j)) {
+                start[i] = j;
+                i--;
+            }
+            j--;
+        }
+        i = 0;
+        j = 0;
+        while (i < n && j < m) {
+            if (skill.charAt(i) == station.charAt(j)) {
+                end[i] = j;
+                i++;
+            }
+            j++;
+        }
+        int max = 0;
+        for (int x = 0; x < n - 1; x++) {
+            max = Math.max(max, start[x+1] - end[x]);
+        }
+        return max;
+
+    }
+}
