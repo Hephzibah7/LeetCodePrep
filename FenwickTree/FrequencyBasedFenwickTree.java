b7b3cd71b3a9b0f37b33de942458238dd774f9b3
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-09-05 18:17:05 +0530

    yellow

--- FenwickTree/FrequencyBasedFenwickTree.java
+++ FenwickTree/FrequencyBasedFenwickTree.java
@@ -1,16 +1,22 @@
 package FenwickTree;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 /*
-search the sum between two intervals
-update an element of the array and get the updated sum
+This is used to count elements less than a given value.
+
+Count the number of elements less than or equal to x
+Count the number of elements between x and y
+
+Let’s try to build tree with assumption that input array have only positive and unique elements.
+Find out the max value in the input array.
+We will use MAX_VALUE+1 as size of Fenwick tree.
+Here index in tree will represent the all possible elements.
+Each element’s weight be considered as 1 therefore partial sum will be nothing but counting the frequency.
 */
-public class ValueBasedFenwickTree {
+import java.io.IOException;
+
+public class FrequencyBasedFenwickTree {
     public int arr[];
 
-    public ValueBasedFenwickTree(int n){
+    public FrequencyBasedFenwickTree(int n){
         arr=new int[n+1];
     }
 
@@ -60,7 +66,14 @@ public class ValueBasedFenwickTree {
         val=index-val;
         return val;
     }
-   
-
-
+     public static void main(String[] args){
+        int max=0;
+        int data[]={6,9,1,8,4};
+        for(int i=0; i<5; i++) max=Math.max(max,data[i]);
+        FrequencyBasedFenwickTree tree=new FrequencyBasedFenwickTree(max);
+        for(int i=0; i<5; i++) tree.insert(1,data[i]);
+        System.out.println("hello"+" "+max+" "+tree.arr.length);
+        tree.display();
+     }
+    
 }
